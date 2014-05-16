4c965762b6ada5035e50cf5c1cc7ee48e2f06e22
bsoufflet <benjamin@fruitionsciences.com>
2014-05-16 18:15:29 +0200

    feat(growthRate): change unit labels to include /m2.

--- library/Fruition/Measure/SpeedByHeat.php
+++ library/Fruition/Measure/SpeedByHeat.php
@@ -42,12 +42,12 @@ class Fruition_Measure_SpeedByHeat extends Zend_Measure_Abstract
      * @var array
      */
     protected $_units = array(
-        'METER_PER_GROWING_DEGREE_DAY_CELSIUS'         => array('1',                                         'm/TT'),
-        'METER_PER_GROWING_DEGREE_DAY_FAHRENHEIT'      => array(array('' => '1', '/' => '0.5555555'),        'm/TTF'),
-        'CENTIMETER_PER_GROWING_DEGREE_DAY_CELSIUS'    => array('0.01',                                      'cm/TT'),
-        'CENTIMETER_PER_GROWING_DEGREE_DAY_FAHRENHEIT' => array(array('' => '0.01', '/' => '0.5555555'),     'cm/TTF'),
-        'INCH_PER_GROWING_DEGREE_DAY_CELSIUS'          => array('0.0254',                                    'in/TT'),
-        'INCH_PER_GROWING_DEGREE_DAY_FAHRENHEIT'       => array(array('' => '0.0254', '/' => '0.5555555'),   'in/TTF'),
+        'METER_PER_GROWING_DEGREE_DAY_CELSIUS'         => array('1',                                         'm/TT/m2'),
+        'METER_PER_GROWING_DEGREE_DAY_FAHRENHEIT'      => array(array('' => '1', '/' => '0.5555555'),        'm/TTF/m2'),
+        'CENTIMETER_PER_GROWING_DEGREE_DAY_CELSIUS'    => array('0.01',                                      'cm/TT/m2'),
+        'CENTIMETER_PER_GROWING_DEGREE_DAY_FAHRENHEIT' => array(array('' => '0.01', '/' => '0.5555555'),     'cm/TTF/m2'),
+        'INCH_PER_GROWING_DEGREE_DAY_CELSIUS'          => array('0.0254',                                    'in/TT/m2'),
+        'INCH_PER_GROWING_DEGREE_DAY_FAHRENHEIT'       => array(array('' => '0.0254', '/' => '0.5555555'),   'in/TTF/m2'),
         'STANDARD'                          => 'METER_PER_GROWING_DEGREE_DAY_CELSIUS'
     );
 }
